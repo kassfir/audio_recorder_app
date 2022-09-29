696f90cc0ffa920bca48e269fb3a5400627f06fd
kassfir <kasparseglitisriga@gmail.com>
2022-09-29 16:49:10 +0300

    Added primitive directory content lister

--- lib/file_list_screen.dart
+++ lib/file_list_screen.dart
@@ -1,14 +1,49 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:path_provider/path_provider.dart';
 
-class FileListScreen extends StatelessWidget {
+class FileListScreen extends StatefulWidget {
   const FileListScreen({Key? key}) : super(key: key);
 
   @override
+  State<FileListScreen> createState() => _FileListScreenState();
+}
+
+class _FileListScreenState extends State<FileListScreen> {
+  String? directory;
+
+  List file = [];
+
+  @override
+  void initState() {
+    _listOfFiles();
+    super.initState();
+  }
+
+  void _listOfFiles() async {
+    directory = (await getApplicationDocumentsDirectory()).path;
+
+    setState(() {
+      file = Directory(directory!).listSync();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('List of Files'),
+    return Container(
+      child: Column(children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: file.length,
+            itemBuilder: ((context, index) {
+              return Text(file[index].toString());
+            }),
+          ),
+        ),
+      ]),
     );
   }
 }
