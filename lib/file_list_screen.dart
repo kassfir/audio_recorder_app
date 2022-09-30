ce4f3919f1bab00ff36d912c8da288ac102b4395
kassfir <kasparseglitisriga@gmail.com>
2022-09-30 19:27:26 +0300

    headers are now added to the saveFile function. Saved files can now be played

--- lib/file_list_screen.dart
+++ lib/file_list_screen.dart
@@ -1,8 +1,10 @@
+import 'dart:developer';
+import 'dart:ffi';
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:path_provider/path_provider.dart';
 
 class FileListScreen extends StatefulWidget {
@@ -15,7 +17,10 @@ class FileListScreen extends StatefulWidget {
 class _FileListScreenState extends State<FileListScreen> {
   String? directory;
 
-  List file = [];
+  AudioPlayer player = AudioPlayer();
+  String? _currentPath;
+
+  List<FileSystemEntity> file = [];
 
   @override
   void initState() {
@@ -27,23 +32,119 @@ class _FileListScreenState extends State<FileListScreen> {
     directory = (await getApplicationDocumentsDirectory()).path;
 
     setState(() {
-      file = Directory(directory!).listSync();
+      file = Directory(directory!)
+          .listSync()
+          .where((element) => getFileExtension(element).length < 5)
+          .toList();
     });
+  }
+
+  void _setAudioPath(String path) async {
+    log(path);
+
+    setState(() {
+      _currentPath = path;
+    });
+
+    Uint8List bytes = await File.fromUri(Uri.file(_currentPath!)).readAsBytes();
+
+    print(bytes);
+
+    await player.play(BytesSource(bytes));
+
+    // await player.setSourceDeviceFile(path);
+
+    // await player.play();
+
+    // final duration = await player.getDuration();
+
+    // log(duration!.inSeconds.toString());
+  }
+
+  String getFileExtension(FileSystemEntity file) {
+    return file.path.split('.').last;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: file.length,
-            itemBuilder: ((context, index) {
-              return Text(file[index].toString());
-            }),
-          ),
+    return Column(children: [
+      Expanded(
+        child: ListView.builder(
+          itemCount: file.length,
+          itemBuilder: ((context, index) {
+            if (file.length == 0) {
+              return Center(
+                child: Text('No files to display'),
+              );
+            }
+
+            return ListTile(
+              title: Text(file[index].path),
+              subtitle: FutureBuilder(
+                future: file[index].stat(),
+                builder: (context, snapshot) {
+                  log('running');
+                  if (snapshot.hasData) {
+                    FileStat stats = snapshot.data as FileStat;
+                    return Text(stats.size.toString());
+                  }
+                  return SizedBox.shrink();
+                },
+              ),
+              onTap: () {
+                if (player.state != PlayerState.playing) {
+                  _setAudioPath(file[index].path);
+                  return;
+                }
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Cannot change source while playing file!'),
+                  ),
+                );
+              },
+              onLongPress: () async {
+                try {
+                  final fileRef = await File(file[index].path);
+                  await fileRef.delete();
+
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Deleted')));
+
+                  _listOfFiles();
+                } catch (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Cound not delete file')));
+
+                  log(error.toString());
+                }
+              },
+            );
+          }),
         ),
-      ]),
-    );
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Play',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              'Pause',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 20,
+      )
+    ]);
   }
 }
--- lib/record_screen.dart
+++ lib/record_screen.dart
@@ -2,11 +2,14 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:core';
 import 'dart:developer';
+import 'dart:typed_data';
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 import 'package:mic_stream/mic_stream.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:uuid/uuid.dart';
 
 class RecordScreen extends StatefulWidget {
   const RecordScreen({Key? key}) : super(key: key);
@@ -27,25 +30,27 @@ class RecordScreenState extends State<RecordScreen> {
   bool _isRecording = false;
   bool _isInitializing = true;
   Stream<List<int>>? stream;
-  List<int> _bytes = [];
-
+  // Uint8List _bytes = Uint8List.fromList([]);
+  final List<int> _bytes = [];
   StreamSubscription<List<int>>? streamSubscription;
+  final AudioPlayer player = AudioPlayer();
 
-  @override
-  void initState() {
-    super.initState();
-  }
+  final SAMPLE_RATE = 44100;
 
   @override
   void didChangeDependencies() async {
     if (_isInitializing) {
-      stream = await MicStream.microphone(sampleRate: 44100);
+      stream = await MicStream.microphone(
+        sampleRate: SAMPLE_RATE,
+        channelConfig: ChannelConfig.CHANNEL_IN_MONO,
+        audioFormat: AudioFormat.ENCODING_PCM_16BIT,
+      );
       streamSubscription = stream!.listen((samples) {
         if (_isRecording) {
+          // _bytes.addAll(samples);
           _bytes.addAll(samples);
           setState(() {});
         }
-        ;
       });
 
       setState(() {
@@ -67,10 +72,56 @@ class RecordScreenState extends State<RecordScreen> {
   }
 
   void _saveFile() async {
-    var directory = await getApplicationDocumentsDirectory();
-    var filePath = '${directory.path}/record.wav';
+    final directory = await getApplicationDocumentsDirectory();
+    final fileName = Uuid().v1();
+    final filePath = '${directory.path}/${fileName}.wav';
 
-    await File(filePath).writeAsBytes(_bytes);
+    final size = _bytes.length;
+    final fileSize = size + 36;
+    final channels = 1;
+    final int byteRate = ((16 * SAMPLE_RATE * channels) / 8).round();
+
+    List<int> fullFile = [
+      // "RIFF"
+      82, 73, 70, 70,
+      fileSize & 0xff,
+      (fileSize >> 8) & 0xff,
+      (fileSize >> 16) & 0xff,
+      (fileSize >> 24) & 0xff,
+      // WAVE
+      87, 65, 86, 69,
+      // fmt
+      102, 109, 116, 32,
+      // fmt chunk size 16
+      16, 0, 0, 0,
+      // Type of format
+      1, 0,
+      // One channel
+      channels, 0,
+      // Sample rate
+      SAMPLE_RATE & 0xff,
+      (SAMPLE_RATE >> 8) & 0xff,
+      (SAMPLE_RATE >> 16) & 0xff,
+      (SAMPLE_RATE >> 24) & 0xff,
+      // Byte rate
+      byteRate & 0xff,
+      (byteRate >> 8) & 0xff,
+      (byteRate >> 16) & 0xff,
+      (byteRate >> 24) & 0xff,
+      // Uhm
+      ((16 * channels) / 8).round(), 0,
+      // bitsize
+      16, 0,
+      // "data"
+      100, 97, 116, 97,
+      size & 0xff,
+      (size >> 8) & 0xff,
+      (size >> 16) & 0xff,
+      (size >> 24) & 0xff,
+      ..._bytes
+    ];
+
+    await File(filePath).writeAsBytes(fullFile);
     log(filePath);
   }
 
@@ -115,6 +166,22 @@ class RecordScreenState extends State<RecordScreen> {
             icon: _isRecording ? Icon(Icons.stop) : Icon(Icons.mic),
             onPressed: () =>
                 _isRecording ? _stopRecording() : _startRecording(),
+            iconSize: 48.0,
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          IconButton(
+            icon: Icon(Icons.play_arrow),
+            onPressed: () {
+              // player.play(UrlSource(
+              //     'https://download.samplelib.com/mp3/sample-3s.mp3'));
+              player.play(
+                BytesSource(
+                  Uint8List.fromList(_bytes),
+                ),
+              );
+            },
             iconSize: 48.0,
           ),
         ],
