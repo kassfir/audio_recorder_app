6e15af0d413fc44cb579e1efacbe424295d40571
kassfir <38891405+kassfir@users.noreply.github.com>
2022-10-24 21:26:36 +0300

    Create config.dart

---
+++ lib/config.dart
@@ -0,0 +1,7 @@
+String ASYA_API_KEY = 'API_KEY';
+String ASYA_ENDPOINT = 'api.asya.ai';
+String ASYA_PATH = 'task_submit';
+String CALLBACK_EMAIL = 'test@test.test';
+
+//For more info on Asya, check the docs: 
+// https://api.asya.ai/docs
